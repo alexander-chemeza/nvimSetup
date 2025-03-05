30cc384cba92601a74718031e997f647ba2ff52a
alexander-chemeza <alexander.chemeza@gmail.com>
2025-03-05 23:58:31 +0300

    added autopairs rule

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,1 +1,6 @@
-require('nvim-autopairs').setup({})
+local Rule = require("nvim-autopairs.rule")
+local npairs = require("nvim-autopairs")
+
+npairs.add_rules({
+	Rule('"""', '"""', "python"), -- Automatically close triple quotes in Python files
+})
