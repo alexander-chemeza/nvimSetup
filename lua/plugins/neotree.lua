3a0fb5854cc6bf57c0474388d548f889edeac97f
Alexander Chemeza <alexander.chemeza@gmail.com>
2025-08-10 15:22:53 +0300

    fixed deprecated options

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,7 +1,11 @@
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+vim.diagnostic.config({
+    signs = {
+        error = { text = " " },
+        warn = { text = " " },
+        info = { text = " " },
+        hint = { text = "󰌵" }
+    }
+})
 
 require("neo-tree").setup({
     close_if_last_window = false,
